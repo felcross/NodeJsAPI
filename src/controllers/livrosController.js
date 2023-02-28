87b81c154b4c2b3ba7a205ae22717a1a69d367be
Felipe Batista <felipe.batista@wdev.int>
2023-02-28 19:24:44 -0300

    finalizando o crud

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -10,7 +10,20 @@ static listarLivros = (req,res) =>{
       })
 }
 
+static listarLivroPorId = (req,res) =>{
+const id =req.params.id
+    
+livros.findById(id,(err, livros)=> {
+    if(err){
+        res.status(400).send({message: `${err.message}+ ID não encontrado`})
+    } else {
+        res.status(200).send(livros)
+    }
+})
+}
+
 static cadastrarLivro = (req,res) =>{
+    //instancia o livro e passa pra ele o que vem no body
 let livro = new livros(req.body)
 
    livro.save((err)=>{
@@ -20,6 +33,31 @@ let livro = new livros(req.body)
         res.status(201).send(livro.toJSON())
     }
    })
+}
+
+static atualizarLivro = (req,res) =>{
+    const id =req.params.id
+
+    livros.findByIdAndUpdate(id,{$set: req.body},(err)=>{
+        if(!err){
+            res.status(200).send({message: "atualização com sucesso"})
+        } else {
+            res.status(500).send({message: `${err.message}+ falha em atulizar `})
+           
+        }
+
+
+    })
+}
+
+static excluirLivro = (req,res) =>{
+    const id =req.params.id
+
+    livros.findByIdAndDelete(id, (err)=>{
+        if(!err)
+         res.status(200).send({message: "Exclusão com sucesso"})
+         else { res.send(500).send({message: `${err.message}+ ID invalido para exclusão`})}
+    })
 
 }
 
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -6,7 +6,10 @@ const router = express.Router();
 
 router
        .get("/livros",LivroController.listarLivros)
+       .get("/livros/:id",LivroController.listarLivroPorId)
        .post("/livros",LivroController.cadastrarLivro)
+       .put("/livros/:id",LivroController.atualizarLivro)
+       .delete("/livros/:id",LivroController.excluirLivro)
 
 
 
